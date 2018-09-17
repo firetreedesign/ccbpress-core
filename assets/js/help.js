5df3b84297b7b9563fce8497a4729d43d61fde03
Daniel Milner <dmilner@gmail.com>
2018-09-17 12:08:00 -0700

    Removed the extra fields from the support beacon and switched to a new beacon id.

--- assets/js/help.js
+++ assets/js/help.js
@@ -19,16 +19,9 @@
     ? e.attachEvent("onload", a)
     : e.addEventListener("load", a, !1);
 })(window, document, window.Beacon || function() {});
-window.Beacon("init", "b8d98d10-d03b-4c26-80fa-babc4e7840c8");
+window.Beacon("init", "7d4c1d76-5a72-4d7a-865a-30b2f5eb48b5");
 
 Beacon("prefill", {
   name: ccbpress_core_beacon_vars.customer_name,
-  email: ccbpress_core_beacon_vars.customer_email,
-  fields: [
-    {
-      "CCBPress Version": ccbpress_core_beacon_vars.ccbpress_ver,
-      "WordPress Version": ccbpress_core_beacon_vars.wp_ver,
-      "PHP Version": ccbpress_core_beacon_vars.php_ver
-    }
-  ]
+  email: ccbpress_core_beacon_vars.customer_email
 });
