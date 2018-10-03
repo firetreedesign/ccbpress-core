91fe597dd469f25b9825a8569555d8ca162cf42b
Daniel Milner <dmilner@gmail.com>
2018-10-02 17:08:22 -0700

    Login block
    * Moved the block PHP code to its own file.
    * Added a block toggle to show/hide the forgot password link.

--- src/login/index.js
+++ src/login/index.js
@@ -11,8 +11,60 @@ import "./editor.scss";
 
 import blockIcons from "../icons.js";
 
+const { Component, Fragment } = wp.element;
 const { __ } = wp.i18n; // Import __() from wp.i18n
 const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
+const { InspectorControls } = wp.editor;
+const { PanelBody, ToggleControl } = wp.components;
+
+class CCBPressLoginBlock extends Component {
+  constructor() {
+    super(...arguments);
+  }
+
+  render() {
+    const { attributes, setAttributes, className } = this.props;
+    const { showForgotPassword } = attributes;
+
+    const inspectorControls = (
+      <InspectorControls key="inspector">
+        <PanelBody title={__("Settings")}>
+          <ToggleControl
+            label={__("Show Forgot Password")}
+            checked={showForgotPassword}
+            onChange={() =>
+              setAttributes({ showForgotPassword: !showForgotPassword })
+            }
+          />
+        </PanelBody>
+      </InspectorControls>
+    );
+
+    return (
+      <Fragment>
+        {inspectorControls}
+        <div className={className} title="title">
+          <form class="ccbpress-core-login" method="post" target="_blank">
+            <fieldset disabled="true">
+              <label>{__("Username:")}</label>
+              <input type="text" value="" />
+              <label>{__("Password:")}</label>
+              <input type="password" value="" />
+              <input type="submit" value={__("Login")} />
+            </fieldset>
+          </form>
+          {showForgotPassword && (
+            <p>
+              <a href="#" onClick="return false;">
+                {__("Forgot username or password?")}
+              </a>
+            </p>
+          )}
+        </div>
+      </Fragment>
+    );
+  }
+}
 
 /**
  * Register: aa Gutenberg Block.
@@ -36,36 +88,15 @@ registerBlockType("ccbpress/login", {
   supports: {
     html: false
   },
-
-  edit: props => {
-    return (
-      <div className={props.className} title="title">
-        <form class="ccbpress-core-login" method="post" target="_blank">
-          <fieldset disabled="true">
-            <label>{__("Username:")}</label>
-            <input type="text" value="" />
-            <label>{__("Password:")}</label>
-            <input type="password" value="" />
-            <input type="submit" value={__("Login")} />
-          </fieldset>
-        </form>
-        <p>
-          <a href="#" onClick="return false;">
-            {__("Forgot username or password?")}
-          </a>
-        </p>
-      </div>
-    );
+  attributes: {
+    showForgotPassword: {
+      type: "boolean",
+      default: true
+    }
   },
 
-  /**
-   * The save function defines the way in which the different attributes should be combined
-   * into the final markup, which is then serialized by Gutenberg into post_content.
-   *
-   * The "save" property must be specified and must be a valid function.
-   *
-   * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-   */
+  edit: CCBPressLoginBlock,
+
   save() {
     // Rendering in PHP
     return null;
