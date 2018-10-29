5dc9b613e9f30ecfb19d6619db6027db61a721b8
Daniel Milner <dmilner@gmail.com>
2018-10-29 14:22:47 -0700

    Fixed a spelling mistake

--- src/components/ccbpress-group-select.js
+++ src/components/ccbpress-group-select.js
@@ -5,32 +5,32 @@ const { Component } = wp.element;
 const { SelectControl } = wp.components;
 
 export default class CCBPressGroupSelect extends Component {
-  constructor(props) {
-    super(...arguments);
-    this.state = {
-      options: [{ id: "", name: __("Loading groups..") }]
-    };
+	constructor(props) {
+		super(...arguments);
+		this.state = {
+			options: [{ id: "", name: __("Loading groups...") }]
+		};
 
-    getGroups()
-      .then(response => response.json())
-      .then(options => {
-        options.unshift({ id: "", name: "" });
-        // console.log(options);
-        return this.setState({ options });
-      });
-  }
+		getGroups()
+			.then(response => response.json())
+			.then(options => {
+				options.unshift({ id: "", name: "" });
+				// console.log(options);
+				return this.setState({ options });
+			});
+	}
 
-  render() {
-    return (
-      <SelectControl
-        type="number"
-        label={__("Please select a group:")}
-        value={this.props.value}
-        onChange={this.props.onChange}
-        options={this.state.options.map(option => {
-          return { value: option.id, label: option.name };
-        })}
-      />
-    );
-  }
+	render() {
+		return (
+			<SelectControl
+				type="number"
+				label={__("Please select a group:")}
+				value={this.props.value}
+				onChange={this.props.onChange}
+				options={this.state.options.map(option => {
+					return { value: option.id, label: option.name };
+				})}
+			/>
+		);
+	}
 }
